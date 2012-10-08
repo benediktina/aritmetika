07e027a08814b43fb6b26781e7499c093fa21f5e
Juliana Beliak <benediktina@gmail.com>
2012-10-08 23:29:20 +0300

    dalyba plius pataisymai

--- main.cpp
+++ main.cpp
@@ -2,23 +2,28 @@
 using namespace std;
 
 int sudetis(int x, int y){
-return x+y
+return x+y;
 }
 int atimtis(int x, int y){
-return x-y
+return x-y;
 }
 int daugyba(int x, int y){
-return x*y
+return x*y;
+}
+int dalyba(int x, int y){
+return x/y;
 }
 
 int main (int argc, char const *argv[])
 {
     int x=10, y=5;
 
-	cout << "Sveika aritmetika!";
-	cout<<"x+y="<<sudetis(x,y);
-        cout<<"x-y="<<atimtis(x,y);
-        cout<<"x*y="<<daugyba(x,y);
+	    cout << "Sveika aritmetika!"<<endl;
+	    cout<<"x+y="<<sudetis(x,y)<<endl;
+        cout<<"x-y="<<atimtis(x,y)<<endl;
+        cout<<"x*y="<<daugyba(x,y)<<endl;
+        cout<<"x/y="<<dalyba(x,y)<<endl;
+        system ("PAUSE");
 	return 0;
 
 }
